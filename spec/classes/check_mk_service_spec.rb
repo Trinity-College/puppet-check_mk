b0bfae87547aaf06dc3a4278260a7888c1bde84a
Steve Clark <xalimar@gmail.com>
2016-04-28 16:37:35 -0700

    Fix test cases around modified code

--- spec/classes/check_mk_service_spec.rb
+++ spec/classes/check_mk_service_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 describe 'check_mk::service', :type => :class do
   context 'with defaults for all parameters' do
+    let(:params) { {
+      :checkmk_service => 'omd',
+      :httpd_service => 'httpd',
+    } }
     it { should contain_class('check_mk::service') }
     it { should contain_service('httpd').with({
           :ensure => 'running',
